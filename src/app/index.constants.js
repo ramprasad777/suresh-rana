ec376fd71f7aa37b6f4220276ce0e6fa8e78fc13
ram <rpaneja@gmail.com>
2016-06-19 17:26:56 +0530

    first commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('sureshRana')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
