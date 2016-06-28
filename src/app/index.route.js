fc7d00c992f64f2201c2c68b46266d1e856d558c
ram <rpaneja@gmail.com>
2016-06-28 23:28:45 +0530

    update pages base frame

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,42 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/templates/about.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('news', {
+        url: '/news',
+        templateUrl: 'app/templates/news.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('events', {
+        url: '/events',
+        templateUrl: 'app/templates/events.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('volunteer', {
+        url: '/voluteer',
+        templateUrl: 'app/templates/volunteers.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('gallery', {
+        url: '/gallery',
+        templateUrl: 'app/templates/gallery.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('contact-us', {
+        url: '/contact-us',
+        templateUrl: 'app/templates/contact-us.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
