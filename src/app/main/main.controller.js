b2d022b0ee5811934dfe8845941223cd819eb321
ram <rpaneja@gmail.com>
2016-07-23 17:42:52 +0530

    read more button updates

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -12,13 +12,18 @@
     vm.awesomeThings = [];
     vm.records=[
     {name:'Suresh Rana',
-    'image':'assets/images/suresh-rana.jpg'
+    'image':'assets/images/suresh-rana.jpg',
+    'thumbUrl': 'https://farm6.staticflickr.com/5830/20552523531_ef720cd2f1_s.jpg',
+    'caption': 'This image has dimensions 2048x1519 and the img element is scaled to fit inside the window.'
     },
     {name:'ram',
-    'image':'assets/images/suresh-rana.jpg'
+    'image':'assets/images/suresh-rana.jpg',
+    'thumbUrl': 'https://farm8.staticflickr.com/7300/12807911134_ff56d1fb3b_s.jpg'
     },
     {name:'ram',
-    'image':'assets/images/suresh-rana.jpg'
+    'image':'assets/images/suresh-rana.jpg',
+    'thumbUrl': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_s.jpg',
+    'caption': 'The left and right arrow keys are binded for navigation. The escape key for closing the modal is binded by AngularUI Bootstrap.'
     },
     {name:'ram',
     'image':'assets/images/suresh-rana.jpg'
